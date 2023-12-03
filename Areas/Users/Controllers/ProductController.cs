b3d7ad879e8b379d31a65046c05f3d9b21160f35
DoAnOOP <phongnguyenhoai2003@gmail.com>
2023-12-03 23:17:39 +0700

    da them giao dien user chua code chuc nang

---
+++ Areas/Users/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test1.Areas.Users.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Users/Product
+        public ActionResult Product2()
+        {
+            return View();
+        }
+        public ActionResult categorie()
+        {
+            return View();
+        }
+        public ActionResult cart()
+        {
+            return View();
+        }
+        public ActionResult checkout()
+        {
+            return View();
+        }
+    }
+}
