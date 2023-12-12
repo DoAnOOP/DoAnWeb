4e4a83614baf23993e2a055bba669c379bd883e8
DoAnOOP <phongnguyenhoai2003@gmail.com>
2023-12-12 19:57:51 +0700

    da them giao dien cua tam

--- Areas/Users/Controllers/ProductController.cs
+++ Areas/Users/Controllers/ProductController.cs
@@ -1,61 +1,67 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using test1.Models;
 
 namespace test1.Areas.Users.Controllers
 {
     public class ProductController : Controller
     {
         // GET: Users/Product
+
+        public DatabaseDataContext db = new DatabaseDataContext();
+
         public ActionResult Product2()
         {
-            if (System.Web.HttpContext.Current.Session["user"] != null)
-            {
-                if ((bool)Session["user"])
-                {
-                    //trường hợp đã login rồi
-                    return View();
-                }
-            }
-            return Redirect("~/TaiKhoan/DangNhap"); 
+            return View();
         }
         public ActionResult categorie()
         {
-            if (System.Web.HttpContext.Current.Session["user"] != null)
-            {
-                if ((bool)Session["user"])
-                {
-                    //trường hợp đã login rồi
-                    return View();
-                }
-            }
-            return Redirect("~/TaiKhoan/DangNhap");
+            return View();
         }
         public ActionResult cart()
         {
-            if (System.Web.HttpContext.Current.Session["user"] != null)
-            {
-                if ((bool)Session["user"])
-                {
-                    //trường hợp đã login rồi
-                    return View();
-                }
-            }
-            return Redirect("~/TaiKhoan/DangNhap");
+            return View();
         }
         public ActionResult checkout()
         {
-            if (System.Web.HttpContext.Current.Session["user"] != null)
+            return View();
+        }
+
+        public string get_ProductsByCategory()
+        {
+            string category = Request["category"];
+            APIResult_ett<List<Product>> rs = new APIResult_ett<List<Product>>();
+            try
             {
-                if ((bool)Session["user"])
+                //truy vấn db để lấy toàn bộ dữ liệu về ds sản phẩm
+                var qr = db.Products.Where(o => o.CategoryName == category);
+                if (qr.Any())
                 {
-                    //trường hợp đã login rồi
-                    return View();
+                    //có dữ liệu => chính là dssv
+                    rs.ErrCode = EnumErrCode.Success;
+                    rs.ErrDesc = "Lấy DSSP thành công";
+                    rs.Data = qr.ToList();
+                }
+                else
+                {
+                    //không có dữ liệu thỏa mãn
+                    rs.ErrCode = EnumErrCode.Empty;
+                    rs.ErrDesc = "DSSP rỗng";
+                    rs.Data = null;
                 }
             }
-            return Redirect("~/TaiKhoan/DangNhap");
+            catch (Exception ex)
+            {
+                rs.ErrCode = EnumErrCode.Error;
+                rs.ErrDesc = "Có lỗi xảy ra trong quá trình lấy về DSSP. Chi tiết lỗi: " + ex.Message;
+                rs.Data = null;
+            }
+
+            return JsonConvert.SerializeObject(rs);
         }
     }
 }
